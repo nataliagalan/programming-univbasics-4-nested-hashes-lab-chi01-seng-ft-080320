bd40cc9ee45974c2807a62cfddafede49107a890
Natalia Galan <n8galan@gmail.com>
2020-07-02 22:01:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 def hopper
 	programmer_hash =
  		{
@@ -133,6 +133,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
- programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
- binding.pry
+ programmer_hash[:dennis_ritchie][:languages[1]] = "Assembly"
+
 end
